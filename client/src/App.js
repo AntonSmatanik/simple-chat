f35e7eb0175ec64bddaf40c12e98c9a3d921004f
Anton Smatanik <smatanik.anton@gmail.com>
2024-09-27 12:05:17 +0200

    simple chat demo

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { ApolloProvider } from "@apollo/client";
+import { useState } from "react";
+import Chat from "./components/Chat";
+import LoginForm from "./components/LoginForm";
+import NavBar from "./components/NavBar";
+import { getUser, logout } from "./lib/auth";
+import { apolloClient } from "./lib/graphql/client";
+
+function App() {
+  const [user, setUser] = useState(getUser);
+
+  const handleLogout = () => {
+    logout();
+    setUser(null);
+  };
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <header>
+        <NavBar user={user} onLogout={handleLogout} />
+      </header>
+      <main>
+        {Boolean(user) ? <Chat user={user} /> : <LoginForm onLogin={setUser} />}
+      </main>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/lib/graphql/hooks.js
@@ -0,0 +1,39 @@
+import { useMutation, useQuery, useSubscription } from "@apollo/client";
+import {
+  addMessageMutation,
+  messageAddedSubscription,
+  messagesQuery,
+} from "./queries";
+
+export function useAddMessage() {
+  const [mutate] = useMutation(addMessageMutation);
+
+  const addMessage = async (text) => {
+    const {
+      data: { message },
+    } = await mutate({
+      variables: { text },
+    });
+    return message;
+  };
+
+  return { addMessage };
+}
+
+export function useMessages() {
+  const { data } = useQuery(messagesQuery);
+
+  useSubscription(messageAddedSubscription, {
+    onData: ({ client, data }) => {
+      client.cache.updateQuery({ query: messagesQuery }, ({ messages }) => {
+        return {
+          messages: [...messages, data.data.message],
+        };
+      });
+    },
+  });
+
+  return {
+    messages: data?.messages ?? [],
+  };
+}
